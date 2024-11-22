631d475b86e3e64abaedff3f4359e2e43700936b
Larissa Oliveira <worklarissaoliveira@gmail.com>
2024-11-22 01:55:36 -0300

    build:building activities and organizing

---
+++ EcoEnergy/app/src/main/java/com/larissa/ecoenergy/TelaMenu.kt
@@ -0,0 +1,20 @@
+package com.larissa.ecoenergy
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TelaMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tela_menu)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
